0843a78d8f3a46974c6c1908075e3e44e5ea5aec
tarahform <ptarahform@gmail.com>
2018-04-17 19:57:28 -0500

    tuesday

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+var wordList = [
+    "Tom Haverford",
+    "Ben Wyatt",
+    "Leslie Knope",
+    "Jerry Gergich",
+    "Ron Swanson",
+    "Donna Meagle",
+    "Andy Dwyer",
+    "April Ludgate",
+    "Chris Traeger",
+    "Ann Perkins",
+    "Jeremy Jamm",
+    "Greg Pikitis",
+    "Mona-Lisa Saperstein",
+    "Jean-Ralphio Saperstein",
+    "Mark Brendanawicz",
+    "The Douche",
+    "Tammy Swanson",
+    "Joan Callamezzo",
+    "Perd Hapley",
+    "Li’l Sebastian"]
+
+console.log(wordList.length);
+
+var randomNumber = Math.floor(math.random() * wordList.length)
+var word = wordList[randomNumber]
